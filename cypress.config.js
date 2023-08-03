673f32af773613173dd417b7fa6b86c327064cfc
Anna Filina <afilina@gmail.com>
2023-08-03 10:28:53 -0400

    Start testing on PHP 8.1

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
     e2e: {
         specPattern: ['tests/e2e/*.cy.{js,jsx,ts,tsx}'],
         supportFile: false,
-        baseUrl: 'http://localhost:8053',
+        baseUrl: 'http://localhost:8081',
     }
 });
