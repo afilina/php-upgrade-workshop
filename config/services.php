789a3a28ea890536bb5cdf3c659cf6d7d19061e9
Anna Filina <afilina@gmail.com>
2023-08-04 15:10:45 -0400

    Configure Docker

--- config/services.php
+++ config/services.php
@@ -2,6 +2,11 @@
 declare(strict_types=1);
 
 use Config\ViewGlobals;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Tools\DsnParser;
+use Products\DoctrineProductRepository;
+use Products\ProductRepository;
 use Twig\AppExtension;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
@@ -13,9 +18,15 @@ $twig = new Environment($loader, [
 ]);
 $twig->addExtension(new AppExtension());
 
+$dsnParser = new DsnParser();
+$connectionParams = $dsnParser->parse('mysqli://root:root@db/app');
+$doctrine = DriverManager::getConnection($connectionParams);
+
 $builder = new DI\ContainerBuilder();
 $builder->addDefinitions([
     ViewGlobals::class => new ViewGlobals('http://localhost:8081'),
-    Environment::class => $twig
+    Environment::class => $twig,
+    Connection::class => $doctrine,
+    ProductRepository::class => DI\autowire(DoctrineProductRepository::class),
 ]);
 return $builder->build();
