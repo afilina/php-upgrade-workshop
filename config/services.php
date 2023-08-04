aed086c36a43e436062c30eaad00c45345c13718
Anna Filina <afilina@gmail.com>
2023-08-04 15:18:14 -0400

    Add view globals to Twig so we no longer need to inject into controllers

--- config/services.php
+++ config/services.php
@@ -11,12 +11,16 @@ use Twig\AppExtension;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
+$viewGlobals = new ViewGlobals('http://localhost:8081');
+
 $loader = new FilesystemLoader(__DIR__ . '/../templates');
 $twig = new Environment($loader, [
     'cache' => __DIR__ . '/../var/cache/templates',
     'debug' => true,
+    'strict_variables' => true,
 ]);
 $twig->addExtension(new AppExtension());
+$twig->addGlobal('global', $viewGlobals);
 
 $dsnParser = new DsnParser();
 $connectionParams = $dsnParser->parse('mysqli://root:root@db/app');
@@ -24,7 +28,7 @@ $doctrine = DriverManager::getConnection($connectionParams);
 
 $builder = new DI\ContainerBuilder();
 $builder->addDefinitions([
-    ViewGlobals::class => new ViewGlobals('http://localhost:8081'),
+    ViewGlobals::class => $viewGlobals, // remove once everything uses Twig
     Environment::class => $twig,
     Connection::class => $doctrine,
     ProductRepository::class => DI\autowire(DoctrineProductRepository::class),
